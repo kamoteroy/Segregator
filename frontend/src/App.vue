f0184ad323948fd37ca560d924cb6837a2db6e41
kamoteroy <kamoteroy6@gmail.com>
2025-05-11 08:53:24 +0800

    secure

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -108,10 +108,8 @@
 import { ref, onMounted, computed, watch } from "vue";
 import { createClient } from "@supabase/supabase-js";
 
-const SUPABASE_URL = "https://gzjxxpeofotelxrzblez.supabase.co";
-const SUPABASE_KEY =
-	"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd6anh4cGVvZm90ZWx4cnpibGV6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzcxMDEwODEsImV4cCI6MjA1MjY3NzA4MX0.R_tCibbHI78B0JYvIja4aNam3tltG3M-eDnmQKn15Cg";
-
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
+const SUPABASE_KEY = import.meta.env.VITE_SUPABASE_KEY;
 const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
 
 const lastValue = ref(null);
